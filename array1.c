f102384d3e900aea107fe54c0340492d3a0cf664
Nathaniel Adraino Saavedra <vedranlol06@gmail.com>
2017-01-31 08:48:15 +0800

    Create array1.c

---
+++ array1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+		int i, grades[3];
+		float average = 0.0;
+		for(i=0; i<3; i++){
+			printf("Enter grade %d: ", i+1);
+			scanf("%d", &grades[i]); 
+		}
+		printf("The grades are: \n");
+		for(i=0, i<3, i++){
+			sum += grades[i];
+		}
+		average = sum/3;
+		printf("The average is: %.2f");
+			if ()
+		
+}
